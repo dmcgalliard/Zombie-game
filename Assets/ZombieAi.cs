49aa3de075c7520ce21f146cc3e0373a027bcda9
Schmo103 <yellow_six@icloud.com>
2024-03-22 12:33:33 -0500

    more zombie ai

--- Assets/ZombieAi.cs
+++ Assets/ZombieAi.cs
@@ -6,16 +6,29 @@ public class ZombieAi : MonoBehaviour
 {
     public GameObject Target;
     public float speed = 1.5f;
+    private Animator animator; // Reference to the Animator component
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>(); // Get the Animator component
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(Target.transform);
+        Vector3 oldPosition = transform.position;
         transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
+
+        // Check if the zombie's position has changed
+        if (oldPosition != transform.position)
+        {
+            animator.SetBool("isMoving", true); // Set the "isMoving" boolean to true
+        }
+        else
+        {
+            animator.SetBool("isMoving", false); // Set the "isMoving" boolean to false
+        }
     }
 }
