ea68d7bbce629af2e469758a27c02c87bf84467c
Drew M <dsmcgalliard@gmail.com>
2024-04-29 13:54:28 -0500

    bug fixes

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class Enemy : MonoBehaviour
 {
     public int enemyHP = 100;
     public Animator animator;
+    public CapsuleCollider capsuleCollider;
     // Start is called before the first frame update
     public void TakeDamage(int damageAmount)
     {
         enemyHP -= damageAmount;
         if (enemyHP <= 0)
         {
+            capsuleCollider.enabled = false;
             animator.SetTrigger("death");
             Destroy(gameObject, 10);
+           
         }
         else
         {
